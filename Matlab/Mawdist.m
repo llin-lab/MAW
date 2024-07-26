fe02798b5532983ce42e9aea1d550beb55251f00
Jingxuan Zhang <jz259@Jingxuans-MacBook-Pro.local>
2024-07-26 14:01:27 -0400

    Change directory

--- Matlab/Mawdist.m
+++
@@ -1,5 +0,0 @@
-function [dist,gammaij]=Mawdist(dim, supp1, supp2,w1,w2);
-% Compute the MAW distance between two GMM with Gusassian component parameters specified in supp1 and supp2 and prior specified to w1 and w2
-
-pairdist=GaussWasserstein(dim, supp1, supp2);
-[dist,gammaij]=ot(pairdist,w1,w2);
